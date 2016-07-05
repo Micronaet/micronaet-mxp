bfbc803aca7f0d40c7a595be2295b68ceb520e53
nicothebrush <nicola.riolini@gmail.com>
2016-07-05 13:56:11 +0200

    Add page in view

--- bom_compare_derive/derive.py
+++ bom_compare_derive/derive.py
@@ -49,12 +49,44 @@ class MrpBomDerived(osv.osv):
     def load_from_parent(self, cr, uid, ids, context=None):
         ''' Load from parent
         '''
-        # TODO write procedure for load
+        assert len(ids), 'Run only with one BOM!'
+        
+        # ---------------------
+        # Remove current lines:
+        # ---------------------
+        bom_proxy = self.browse(cr, uid, ids, context=context)[0]                
+        unlink_ids = [item.id for item in bom_proxy.bom_lines]
+        self.unlink(cr, uid, unlink_ids, context=context)
+        
+        # ---------------------------------
+        # Create line from parent elements:
+        # ---------------------------------
+        for line in bom_proxy.from_bom_id.bom_lines:
+            self.create(cr, uid, {
+                'bom_id': ids[0],
+                'product_id': line.product_id.id,
+                'product_uom': line.product_uom.id,
+                'product_qty': line.product_qty,
+                'date_start': line.date_start,
+                'date_stop': line.date_stop,                
+                }, context=context)                
         return True
              
     _columns = {
         'from_bom_id': fields.many2one(
             'mrp.bom', 'From BOM', domain=[('bom_id', '=', False)]), 
         }
+
+class MrpBomDerived(osv.osv):
+    ''' Add extra field to manage mrp.bom
+    ''' 
+    _inherit = 'mrp.bom'
+
+    _columns = {
+        'derived_ids': fields.one2many(
+            'mrp.bom', 'from_bom_id', 'Derived BOM'),
+        }
+    
+    
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
